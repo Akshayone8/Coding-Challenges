d2d3e3cc156f70d403eb67d1691ace5753d8c615
Akshay <62467248+Akshayone8@users.noreply.github.com>
2022-10-26 09:37:39 +0530

    Updated

---
+++ Challengs-29.js
@@ -0,0 +1,22 @@
+// 
+
+// #solution
+
+
+const pairs={
+  A:'T',
+  T:'A',
+  C:'G',
+  G:'C'
+}
+
+
+function pairElement(str) {
+
+return str.split('').map(item=>(
+  [item,pairs[item]]
+));
+
+}
+
+pairElement("GCG");
