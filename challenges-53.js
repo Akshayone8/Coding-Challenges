f7a7c856e7e450ce40fb3e524732d60a1837dc0d
Akshay <62467248+Akshayone8@users.noreply.github.com>
2023-12-18 13:00:51 +0530

    added challenges 53 and 54

---
+++ challenges-53.js
@@ -0,0 +1,80 @@
+// #Question 682. Baseball Game
+
+// You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+// You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+// An integer x.
+// Record a new score of x.
+// '+'.
+// Record a new score that is the sum of the previous two scores.
+// 'D'.
+// Record a new score that is the double of the previous score.
+// 'C'.
+// Invalidate the previous score, removing it from the record.
+// Return the sum of all the scores on the record after applying all the operations.
+
+// The test cases are generated such that the answer and all intermediate calculations fit in a 32-bit integer and that all operations are valid.
+
+// Example 1:
+
+// Input: ops = ["5","2","C","D","+"]
+// Output: 30
+// Explanation:
+// "5" - Add 5 to the record, record is now [5].
+// "2" - Add 2 to the record, record is now [5, 2].
+// "C" - Invalidate and remove the previous score, record is now [5].
+// "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+// "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+// The total sum is 5 + 10 + 15 = 30.
+// Example 2:
+
+// Input: ops = ["5","-2","4","C","D","9","+","+"]
+// Output: 27
+// Explanation:
+// "5" - Add 5 to the record, record is now [5].
+// "-2" - Add -2 to the record, record is now [5, -2].
+// "4" - Add 4 to the record, record is now [5, -2, 4].
+// "C" - Invalidate and remove the previous score, record is now [5, -2].
+// "D" - Add 2 * -2 = -4 to the record, record is now [5, -2, -4].
+// "9" - Add 9 to the record, record is now [5, -2, -4, 9].
+// "+" - Add -4 + 9 = 5 to the record, record is now [5, -2, -4, 9, 5].
+// "+" - Add 9 + 5 = 14 to the record, record is now [5, -2, -4, 9, 5, 14].
+// The total sum is 5 + -2 + -4 + 9 + 5 + 14 = 27.
+// Example 3:
+
+// Input: ops = ["1","C"]
+// Output: 0
+// Explanation:
+// "1" - Add 1 to the record, record is now [1].
+// "C" - Invalidate and remove the previous score, record is now [].
+// Since the record is empty, the total sum is 0.
+
+// #Solution
+
+function calPoints(ops) {
+  const stack = [];
+
+  for (const op of ops) {
+    if (op === "C") {
+      stack.pop();
+    } else if (op === "D") {
+      stack.push(2 * stack[stack.length - 1]);
+    } else if (op === "+") {
+      stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+    } else {
+      stack.push(parseInt(op, 10));
+    }
+  }
+
+  return stack.reduce((sum, score) => sum + score, 0);
+}
+
+// Example usage:
+const ops1 = ["5", "2", "C", "D", "+"];
+const ops2 = ["5", "-2", "4", "C", "D", "9", "+", "+"];
+const ops3 = ["1", "C"];
+
+console.log(calPoints(ops1)); // Output: 30
+console.log(calPoints(ops2)); // Output: 27
+console.log(calPoints(ops3)); // Output: 0
