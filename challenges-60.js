29f436f3932c6a30dae1bf562e142096326a78cc
Akshay <62467248+Akshayone8@users.noreply.github.com>
2023-12-22 09:29:42 +0530

    added challenges 59 & 60

---
+++ challenges-60.js
@@ -0,0 +1,39 @@
+// #Questiuon
+// 2108. Find First Palindromic String in the Array
+
+// Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+// A string is palindromic if it reads the same forward and backward.
+
+// Example 1:
+
+// Input: words = ["abc","car","ada","racecar","cool"]
+// Output: "ada"
+// Explanation: The first string that is palindromic is "ada".
+// Note that "racecar" is also palindromic, but it is not the first.
+// Example 2:
+
+// Input: words = ["notapalindrome","racecar"]
+// Output: "racecar"
+// Explanation: The first and only string that is palindromic is "racecar".
+// Example 3:
+
+// Input: words = ["def","ghi"]
+// Output: ""
+// Explanation: There are no palindromic strings, so the empty string is returned.
+
+// #Solution
+
+function isPalindrome(word) {
+  return word === word.split("").reverse().join("");
+}
+var firstPalindrome = function (words) {
+  for (const word of words) {
+    if (isPalindrome(word)) {
+      return word;
+    }
+  }
+  return "";
+};
+
+firstPalindrome(["abc", "car", "ada", "racecar", "cool"]);
