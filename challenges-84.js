448d14f8d64d7d39ff5f1db0585ef5580a501bbe
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-02-13 07:31:45 +0530

    added 84th challenge

---
+++ challenges-84.js
@@ -0,0 +1,19 @@
+//  #CodeWars
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+//Solution
+
+function findShort(s) {
+  let newArr = s.split(" ");
+  let finalResult = [];
+  for (let i = 0; i < newArr.length; i++) {
+    finalResult.push(newArr[i].length);
+  }
+  return Math.min(...finalResult);
+}
+
+findShort("bitcoin take over the world maybe who knows perhaps"); //3
+findShort("Let's travel abroad shall we");
