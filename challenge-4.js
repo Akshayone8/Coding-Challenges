45108f5d4eabf4cdd5c8acc62ba14b30e8c3d6cf
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-02-14 08:29:35 +0530

    addede another method to find the solution which is simple to understand.

--- challenge-4.js
+++ challenge-4.js
@@ -10,7 +10,6 @@
 
 //  Kata.getMiddle("A") should return "A"
 
-
 //  #Input
 
 // A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
@@ -19,25 +18,30 @@
 
 // The middle character(s) of the word represented as a string.
 
-
 // #Solution
 
-function getMiddle(s)
-{
+function getMiddle(s) {
+  let findMiddleChar = Math.floor(s.length / 2);
+  return s.length % 2 === 0
+    ? s.slice(findMiddleChar - 1, findMiddleChar + 1)
+    : s.charAt(findMiddleChar);
+}
+
+//2nd method
+function getMiddle(s) {
   //Code goes here!
   let strIdx;
   let endIdx;
-  
-  if(s.length%2===0){
-    strIdx = Math.floor(s.length/2)-1;
-    endIdx = strIdx+2;
-    
-  }else{
-    strIdx = Math.floor(s.length/2);
-     endIdx = strIdx+1;
+
+  if (s.length % 2 === 0) {
+    strIdx = Math.floor(s.length / 2) - 1;
+    endIdx = strIdx + 2;
+  } else {
+    strIdx = Math.floor(s.length / 2);
+    endIdx = strIdx + 1;
   }
-  return s.substring(strIdx,endIdx)
+  return s.substring(strIdx, endIdx);
 }
 
-getMiddle('test'); //even 
-getMiddle('tests') //odd
+getMiddle("test"); //even
+getMiddle("tests"); //odd
