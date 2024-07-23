e509073e814ef638689d37aaa42dae347a900e58
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-07-23 12:37:26 +0530

    changed 23rd challenged

--- Challengs-19.js
+++ Challengs-19.js
@@ -7,11 +7,10 @@
 // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
-
 // #solution
 
-function addBinary(a,b) {
-  return (a+b).toString(2);
+function addBinary(a, b) {
+  return (a + b).toString(2);
 }
 
-addBinary(1,2)
+addBinary(1, 2);
--- Challengs-23.js
+++ Challengs-23.js
@@ -19,18 +19,18 @@
 
 // #solution
 
-function points(games) {
-  // your code here
-  
-  return games.reduce((acc,game)=>{
-    const [x,y]=game.split(':').map(Number) // 1:0=>["1","0"]
-    if(x>y){
-      acc+=3;
-    }else if(x===y){
-      acc+=1;
+function points(arr) {
+  let total = 0;
+  arr.forEach((item) => {
+    let [x, y] = item.split(":");
+    if (x > y) {
+      total += 3;
+    } else if (x === y) {
+      total += 1;
+    } else {
+      total += 0;
     }
-    return acc;
-  },0)
+  });
+  return total;
 }
-
-points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])
+points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]);
